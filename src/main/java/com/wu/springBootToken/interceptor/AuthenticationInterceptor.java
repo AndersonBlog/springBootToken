a1221f1425b1faa8418a6e0df99cfce693f2cef8
wu <1729461046@qq.com>
2019-10-16 23:09:35 +0800

    fix bug

--- src/main/java/com/wu/springBootToken/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/wu/springBootToken/interceptor/AuthenticationInterceptor.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -86,5 +87,15 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             }
         }
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
     }
 }
